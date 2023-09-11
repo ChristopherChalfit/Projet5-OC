3783486442f5bfeb4a2287874913cd113d3d392d
Christopher Chalafit <christopher.chalafit@hotmail.com>
2023-09-11 10:52:28 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,9 +7,6 @@ import { addEventModal } from "./modal.js";
 // serveur local : http://localhost:5678/api/
 document.addEventListener("DOMContentLoaded", function (event) {
   event.preventDefault();
-  const apiUrlWorks = "http://localhost:5678/api/works";
-  const apiUrlCategories = "http://localhost:5678/api/categories";
-  const apiUrlDeleteWork = "http://localhost:5678/api/works/";
 
   let works = window.localStorage.getItem("works");
   let category = window.localStorage.getItem("category");
